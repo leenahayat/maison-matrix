b1888e378be9d05a3d44da85bf59db2f4d821bc5
Leena Hayat <leenahayat14@gmail.com>
2026-07-13 19:58:52 +0500

    Clean env and set dynamic port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const inquiryRoutes = require('./routes/inquiryRoutes');
 
 const app = express();
 
-// Middlewares
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); 
@@ -20,12 +20,9 @@ mongoose.connect(CLOUD_MONGO_URI)
     .then(() => console.log("MongoDB Connected Successfully!"))
     .catch(err => console.log("Database Connection Error:", err));
 
-
-
-// Mount the CRUD API Route
 app.use('/api', inquiryRoutes);
 
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Maison Matrix Server running perfectly on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
