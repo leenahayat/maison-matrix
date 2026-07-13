def2004d77fe37416d8aa83d81a37166105968ed
Leena Hayat <leenahayat14@gmail.com>
2026-07-13 20:37:18 +0500

    Fixing errors

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ global.crypto = require('crypto');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path'); // Added path module
 const inquiryRoutes = require('./routes/inquiryRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/public')));
 
 require('dotenv').config();
 
@@ -19,6 +22,10 @@ mongoose.connect(CLOUD_MONGO_URI)
     .catch(err => console.log("Database Connection Error:", err));
 
 app.use('/api', inquiryRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
