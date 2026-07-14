9d527c1ee7168a6e3ad72c0f00be2812b797f531
Leena Hayat <leenahayat14@gmail.com>
2026-07-14 13:55:02 +0500

    Use rewrites instead of catch-all route

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
 global.crypto = require('crypto');
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path'); 
 const inquiryRoutes = require('./routes/inquiryRoutes');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 require('dotenv').config();
-
-const CLOUD_MONGO_URI = process.env.cloudMongoUrl;
-
-mongoose.connect(CLOUD_MONGO_URI)
-    .then(() => console.log("MongoDB Connected Successfully!"))
-    .catch(err => console.log("Database Connection Error:", err));
+mongoose.connect(process.env.cloudMongoUrl)
+    .then(() => console.log("Connected!"))
+    .catch(err => console.log(err));
 
 app.use('/api', inquiryRoutes);
 
 app.use(express.static(__dirname)); 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
